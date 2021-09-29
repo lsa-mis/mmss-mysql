56573c942adc99bc25f4a2b95b29b561fbe72747
Rick Smoke <rsmoke@umich.edu>
2021-09-29 16:16:25 -0400

    moved css folder contents to stylesheets and updated import and deleted the css folder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("css/application.sass")
-require("css/_footer.sass")
+require("stylesheets/application.sass")
+require("stylesheets/_footer.sass")
 
 import 'controllers'
 
