857fcd25805dc0c7d69c006fb55d686d8074d738
rsmokeUM <rsmoke@umich.edu>
2021-10-19 09:21:53 -0400

    Update camp_configurations.rb

--- app/admin/camp_configurations.rb
+++ app/admin/camp_configurations.rb
@@ -23,7 +23,7 @@ ActiveAdmin.register CampConfiguration do
   controller do
     # Custom new method
     def new
-      flash[:alert] = "Letters texts were copied from the last year camp. Don't forget to edit them."
+      flash[:alert] = "Each letter's text and camp fee were copied from previous camp. Don't forget to edit them."
       @camp_configuration = CampConfiguration.last.dup
     end
   end
