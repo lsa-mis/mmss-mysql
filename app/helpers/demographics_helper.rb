ef3130c6b8e11089664b8b66c567de558921b6d3
Margarita Barvinok <brita@umich.edu>
2021-12-06 16:43:44 -0500

    Fix no-empty-method-for-nil error

--- app/helpers/demographics_helper.rb
+++ app/helpers/demographics_helper.rb
@@ -1,6 +1,6 @@
 module DemographicsHelper
   def demographic_description(applicant_detail)
-    if applicant_detail.demographic.empty?
+    if applicant_detail.demographic.blank?
       'None Selected'
     else
       Demographic.find(applicant_detail.demographic).name
