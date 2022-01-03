d0730fe66b839befeac17ecba4d736899b342bb9
Margarita Barvinok <brita@umich.edu>
2022-01-03 15:24:49 -0500

    make rails_helper work with devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,14 +63,16 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
+# Shoulda::Matchers.configure do |config|
+#   config.integrate do |with|
+#     with.test_framework :rspec
+#     with.library :rails
+#   end
+# end
 
 Capybara.javascript_driver = ENV['SHOW_BROWSER'] ? :selenium_chrome : :selenium_chrome_headless
