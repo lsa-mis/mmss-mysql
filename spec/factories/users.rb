54b9bb3118ecc881ff24bcd52f96d6c68994eba5
Margarita Barvinok <brita@umich.edu>
2022-01-06 12:28:23 -0500

    no faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -18,7 +18,7 @@
 #
 FactoryBot.define do
   factory :user do
-    email {Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 10) }
+    email { "rita@test.com" }
+    password { "secretsecret" }
   end
 end
