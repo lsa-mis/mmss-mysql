5683f1896b5d58f830ec467e2d8648ea67a4a72a
Margarita Barvinok <brita@umich.edu>
2022-02-16 08:15:16 -0500

    Add applicant activites to active admin

--- app/admin/financial_aids.rb
+++ app/admin/financial_aids.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register FinancialAid, as: "Financial Aid Request" do
-  menu parent: 'Applicant Info', priority: 2
+  menu parent: 'Applicant Info', priority: 3
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
