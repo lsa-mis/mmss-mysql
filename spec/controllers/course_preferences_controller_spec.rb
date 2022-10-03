72b21c0acc5123c8590d9dbb2fa375d99757eeae
Margarita Barvinok <brita@umich.edu>
2022-10-03 16:47:33 -0400

    delete unneded spec files

--- spec/controllers/course_preferences_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe CoursePreferencesController, type: :controller do
-
-end
--- spec/mailers/previews/feedback_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/feedback
-class FeedbackPreview < ActionMailer::Preview
-
-end
