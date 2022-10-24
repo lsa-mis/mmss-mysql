e5f866f58a94975b6c729db3c592ffe150b9956e
Margarita Barvinok <brita@umich.edu>
2022-10-24 19:17:06 -0400

    add tests for enrollment model

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -29,5 +29,44 @@
 require 'rails_helper'
 
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    load "#{Rails.root}/spec/test_seeds.rb" 
+  end
+
+  context "all required fields are present" do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:applicant_detail) { FactoryBot.create(:applicant_detail, user: user) }
+    let!(:session_registration_ids) { CampOccurrence.all.pluck(:id)}
+    let!(:course_registration_ids) { Course.where(camp_occurrence_id: session_registration_ids).pluck(:id)}
+
+    it 'test database' do
+      expect(ActiveRecord::Base.connection_config[:database]).to match(/test/)
+    end
+
+    it 'is valid' do
+      expect(FactoryBot.create(:enrollment, user: user, session_registration_ids: session_registration_ids, course_registration_ids: course_registration_ids))
+        .to be_valid
+    end
+
+    it 'not valid without session_registration' do
+      session_registration_ids = []
+      course_registration_ids = []
+      expect { (FactoryBot.create(:enrollment, user: user, session_registration_ids: session_registration_ids, course_registration_ids: course_registration_ids)) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Select at least one session, Select at least one course")
+    end
+
+    it 'not valid without course_registration' do
+      course_registration_ids = []
+      expect { (FactoryBot.create(:enrollment, user: user, session_registration_ids: session_registration_ids, course_registration_ids: course_registration_ids)) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Select at least one course")
+    end
+
+    it 'prevent creating second enrollment records for a user for the current camp' do
+      enroll1 = FactoryBot.create(:enrollment, user: user, session_registration_ids: session_registration_ids, course_registration_ids: course_registration_ids)
+      expect(enroll1).to be_valid
+      expect { (FactoryBot.create(:enrollment, user: user, session_registration_ids: session_registration_ids, course_registration_ids: course_registration_ids)) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: User has already been taken")
+    end
+  end
 end
