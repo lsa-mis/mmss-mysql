d3c61f002636b0c1854b78770173560d3b94f84f
Margarita Barvinok <brita@umich.edu>
2022-12-09 16:11:20 -0500

    redirect after login for faculty, admin or user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,7 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if faculty_signed_in?
-      uniqname = current_faculty.email.split('@').first
-      unless Course.current_camp.pluck(:faculty_uniqname).uniq.compact.include?(uniqname)
-        # flash.now[:alert] = "hell"
-        # redirect_todestroy_faculty_session_path
-        redirect_to(controller: 'faculties/sessions', action: 'destroy', method: :delete) and return
-      end
       faculties_path
-      # uniqname = current_faculty.email.split('@').first
-      # if Course.current_camp.pluck(:faculty_uniqname).uniq.compact.include?(uniqname)
-      #   faculty_path
-      # else
-      #   flash.now[:alert] = "hell"
-      #   destroy_faculty_session_path
-      # end
     elsif admin_signed_in?
       admin_root_path
     else
