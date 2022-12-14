15ce6bd8101ced9dc9f9ec4f7ea422b8b7b6f102
Margarita Barvinok <brita@umich.edu>
2022-12-14 17:15:57 -0500

    Edit travels table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_231309) do
+ActiveRecord::Schema.define(version: 2022_12_14_220020) do
 
   create_table "active_admin_comments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "namespace"
@@ -235,6 +235,23 @@ ActiveRecord::Schema.define(version: 2022_10_15_231309) do
     t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
 
+  create_table "faculties", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_faculties_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_faculties_on_reset_password_token", unique: true
+  end
+
   create_table "feedbacks", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "genre"
     t.string "message"
@@ -365,15 +382,18 @@ ActiveRecord::Schema.define(version: 2022_10_15_231309) do
 
   create_table "travels", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "enrollment_id", null: false
-    t.string "direction"
-    t.string "transport_needed"
-    t.datetime "date"
-    t.string "mode"
-    t.string "carrier"
-    t.string "route_num"
+    t.string "arrival_transport"
+    t.string "arrival_carrier"
+    t.string "arrival_route_num"
     t.text "note"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "arrival_date"
+    t.time "arrival_time"
+    t.string "depart_transport"
+    t.time "depart_route_num"
+    t.date "depart_date"
+    t.time "depart_time"
     t.index ["enrollment_id"], name: "index_travels_on_enrollment_id"
   end
 
