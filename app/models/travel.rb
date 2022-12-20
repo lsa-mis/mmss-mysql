6a6d30a8e545f296fb6158a10d4acfe15e2d322d
Margarita Barvinok <brita@umich.edu>
2022-12-20 16:46:45 -0500

    add validations to travel model

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -22,9 +22,41 @@
 #
 class Travel < ApplicationRecord
   belongs_to :enrollment
+  before_save :check_transport 
 
-  validates :arrival_transport, :arrival_carrier, :arrival_route_num, 
-    :arrival_date, :arrival_time, :depart_transport, :depart_route_num, 
-    :depart_date, :depart_time, :depart_carrier, 
-    :arrival_session, :depart_session, presence: true
+  validates :arrival_transport, :depart_transport, :arrival_session, :depart_session, presence: true
+
+  validates_presence_of :arrival_carrier, :arrival_route_num, :arrival_date, :arrival_time, if: :valid_arrival_transport?
+  validates_presence_of :depart_carrier, :depart_route_num, :depart_date, :depart_time,  if: :valid_depart_transport?
+
+  def valid_arrival_transport?
+    if self.arrival_transport.include?("Automobile") || self.arrival_transport.include?("commuter")
+      false
+    else
+      true
+    end
+  end
+
+  def valid_depart_transport?
+    if self.depart_transport.include?("Automobile") || self.depart_transport.include?("commuter")
+      false
+    else
+      true
+    end
+  end
+
+  def check_transport
+    if self.arrival_transport.include?("Automobile") || self.arrival_transport.include?("commuter")
+      self.arrival_carrier = ''
+      self.arrival_route_num = ''
+      self.arrival_date = ''
+      self.arrival_time = ''
+    end
+    if self.depart_transport.include?("Automobile") || self.depart_transport.include?("commuter")
+      self.depart_carrier = ''
+      self.depart_route_num = ''
+      self.depart_date = ''
+      self.depart_time = ''
+    end
+  end
 end
