975d1550558695ee904776300324dd49c6a083ac
Margarita Barvinok <brita@umich.edu>
2022-12-20 17:23:32 -0500

    restore chack dates

--- app/javascript/controllers/travel_controller.js
+++ app/javascript/controllers/travel_controller.js
@@ -4,24 +4,25 @@ export default class extends Controller {
 
   checkDates(event) {
     console.log("nothing)")
-    // let arrival = this.arrivalTarget.value;
-    // let depart = this.departTarget.value;
 
-    // if (typeof arrival !== 'undefined' && typeof depart !== 'undefined') {
-    //   var d_arrival = Date.parse(arrival)
-    //   var d_depart = Date.parse(depart)
-    //   if (d_depart < d_arrival) {
-    //     this.messageTarget.classList.add("date-error--display")
-    //     this.messageTarget.classList.remove("date-error--hide")
-    //     this.messageTarget.innerText = "Departure date should occur after Arrival date";
-    //     event.preventDefault()
-    //   }
-    //   else {
-    //     this.messageTarget.classList.add("date-error--hide")
-    //     this.messageTarget.classList.remove("date-error--display")
-    //     this.messageTarget.innerText = " "
-    //   }
-    // }
+    let arrival = this.arrivalTarget.value;
+    let depart = this.departTarget.value;
+
+    if (typeof arrival !== 'undefined' && typeof depart !== 'undefined') {
+      var d_arrival = Date.parse(arrival)
+      var d_depart = Date.parse(depart)
+      if (d_depart < d_arrival) {
+        this.messageTarget.classList.add("date-error--display")
+        this.messageTarget.classList.remove("date-error--hide")
+        this.messageTarget.innerText = "Departure date should occur after Arrival date";
+        event.preventDefault()
+      }
+      else {
+        this.messageTarget.classList.add("date-error--hide")
+        this.messageTarget.classList.remove("date-error--display")
+        this.messageTarget.innerText = " "
+      }
+    }
   }
 
   checkArrivalTransport(event) {
