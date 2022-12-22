4d5b605e53159abca36714bdab710e64429d0a9f
Margarita Barvinok <brita@umich.edu>
2022-12-22 14:40:19 -0500

    save working version

--- app/javascript/controllers/travel_controller.js
+++ app/javascript/controllers/travel_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from 'stimulus';
 export default class extends Controller {
-  static targets = ["depart", "arrival", "message", "arrival_transport", "arrrival_details", "depart_transport", "depart_details"]
+  static targets = ["form", "depart", "arrival", "message", "arrival_transport", "arrival_details", "depart_transport", "depart_details",
+    "arrival_date_time", "depart_date_time", "arrival_message",
+    "arrival_carrier", "arrival_route_num", "arrival_time", "arrival_message",
+    "depart_carrier", "depart_route_num", "depart_time", "depart_message",
+  ]
 
   checkDates(event) {
     console.log("nothing)")
@@ -25,29 +29,175 @@ export default class extends Controller {
     }
   }
 
-  checkArrivalTransport(event) {
-    let transport = event.target.value
-    if (transport.includes("Automobile") || transport.includes("commuter")) {
-      this.arrrival_detailsTarget.classList.remove("travel-details--show")
-      this.arrrival_detailsTarget.classList.add("travel-details--hide")
-
+  checkArrivalTransport() {
+    let transport = this.arrival_transportTarget.value;
+    console.log(transport)
+    if (transport.includes("Automobile")) {
+      this.arrival_detailsTarget.classList.remove("travel-details--show")
+      this.arrival_detailsTarget.classList.add("travel-details--hide")
+      this.arrival_date_timeTarget.classList.remove("travel-details--hide")
+      this.arrival_date_timeTarget.classList.add("travel-details--show")
+    }
+    else if (transport.includes("commuter")) {
+      this.arrival_detailsTarget.classList.remove("travel-details--show")
+      this.arrival_detailsTarget.classList.add("travel-details--hide")
+      this.arrival_date_timeTarget.classList.remove("travel-details--show")
+      this.arrival_date_timeTarget.classList.add("travel-details--hide")
     }
     else {
-      this.arrrival_detailsTarget.classList.add("travel-details--show")
-      this.arrrival_detailsTarget.classList.remove("travel-details--hide")
+      this.arrival_detailsTarget.classList.add("travel-details--show")
+      this.arrival_detailsTarget.classList.remove("travel-details--hide")
+      this.arrival_date_timeTarget.classList.add("travel-details--show")
+      this.arrival_date_timeTarget.classList.remove("travel-details--hide")
     }
   }
 
-  checkDepartTransport(event) {
-    let transport = event.target.value
-    if (transport.includes("Automobile") || transport.includes("commuter")) {
+  checkDepartTransport() {
+    console.log("hell");
+
+    let transport = this.depart_transportTarget.value;
+    if (transport.includes("Automobile")) {
       this.depart_detailsTarget.classList.remove("travel-details--show")
       this.depart_detailsTarget.classList.add("travel-details--hide")
+      this.depart_date_timeTarget.classList.remove("travel-details--hide")
+      this.depart_date_timeTarget.classList.add("travel-details--show")
+    }
+    else if (transport.includes("commuter")) {
+      this.depart_detailsTarget.classList.remove("travel-details--show")
+      this.depart_detailsTarget.classList.add("travel-details--hide")
+      this.depart_date_timeTarget.classList.remove("travel-details--show")
+      this.depart_date_timeTarget.classList.add("travel-details--hide")
     }
     else {
       this.depart_detailsTarget.classList.add("travel-details--show")
       this.depart_detailsTarget.classList.remove("travel-details--hide")
+      this.depart_date_timeTarget.classList.add("travel-details--show")
+      this.depart_date_timeTarget.classList.remove("travel-details--hide")
     }
   }
-}
 
+  submitForm(event) {
+    let no_submit = "false";
+    console.log("submit");
+    let arrival_transport = this.arrival_transportTarget.value;
+    let depart_transport = this.depart_transportTarget.value;
+
+    console.log(arrival_transport)
+    console.log(depart_transport)
+
+    if (arrival_transport.includes("commuter")) {
+      this.arrival_carrierTarget.value = "";
+      this.arrival_route_numTarget.value = "";
+      this.arrivalTarget.value = "";
+      this.arrival_timeTarget.value = "";
+    }
+
+    if (arrival_transport.includes("Airplane") || arrival_transport.includes("Train") || arrival_transport.includes("Bus")) {
+      console.log("works");
+      var arrival_carrier = this.arrival_carrierTarget.value;
+      console.log(this.arrival_carrierTarget);
+      console.log(arrival_carrier);
+      var arrival_route_num = this.arrival_route_numTarget.value;
+      let arrival = this.arrivalTarget.value;
+      var arrival_time = this.arrival_timeTarget.value;
+      console.log(arrival_route_num);
+      console.log(arrival);
+      console.log(arrival_time);
+      if (arrival_carrier == "" || arrival_route_num == "" || arrival == "" || arrival_time == "") {
+        console.log("empty");
+        this.arrival_messageTarget.classList.add("date-error--display")
+        this.arrival_messageTarget.classList.remove("date-error--hide")
+        this.arrival_messageTarget.innerText = "Fill out all arrival information";
+        event.preventDefault();
+        event.stopImmediatePropagation()
+      }
+      else {
+        this.arrival_messageTarget.innerText = "";
+        this.arrival_messageTarget.classList.remove("date-error--display")
+        this.arrival_messageTarget.classList.add("date-error--hide")
+      }
+    }
+    if (arrival_transport.includes("Automobile")) {
+      console.log("works");
+      var arrival = this.arrivalTarget.value;
+      var arrival_time = this.arrival_timeTarget.value;
+      if (arrival == "" || arrival_time == "") {
+        console.log("empty");
+        this.arrival_messageTarget.classList.add("date-error--display")
+        this.arrival_messageTarget.classList.remove("date-error--hide")
+        this.arrival_messageTarget.innerText = "Fill out all arrival information";
+        event.preventDefault();
+        event.stopImmediatePropagation()
+      }
+      else {
+        this.arrival_messageTarget.innerText = "";
+        this.arrival_messageTarget.classList.remove("date-error--display")
+        this.arrival_messageTarget.classList.add("date-error--hide")
+
+      }
+    }
+
+    if (depart_transport.includes("commuter")) {
+      this.depart_carrierTarget.value = "";
+      this.depart_route_numTarget.value = "";
+      this.departTarget.value = "";
+      this.depart_timeTarget.value = "";
+    }
+
+    if (depart_transport.includes("Airplane") || depart_transport.includes("Train") || depart_transport.includes("Bus")) {
+      console.log("depart works");
+      var depart_carrier = this.depart_carrierTarget.value;
+      console.log(depart_carrier);
+      var depart_route_num = this.depart_route_numTarget.value;
+      var depart = this.departTarget.value;
+      var depart_time = this.depart_timeTarget.value;
+      console.log(depart_route_num);
+      console.log(depart);
+      console.log(depart_time);
+      if (depart_carrier == "" || depart_route_num == "" || depart == "" || depart_time == "") {
+        console.log("depart empty");
+        this.depart_messageTarget.classList.add("date-error--display")
+        this.depart_messageTarget.classList.remove("date-error--hide")
+        this.depart_messageTarget.innerText = "Fill out all depart information";
+        event.preventDefault();
+        event.stopImmediatePropagation()
+      }
+      else {
+        console.log("depart remove");
+        this.depart_messageTarget.innerText = "";
+        this.depart_messageTarget.classList.remove("date-error--display")
+        this.depart_messageTarget.classList.add("date-error--hide")
+
+      }
+    }
+    if (depart_transport.includes("Automobile")) {
+      console.log("works");
+      var depart = this.departTarget.value;
+      var depart_time = this.depart_timeTarget.value;
+      this.depart_carrierTarget.value = "";
+      this.depart_route_numTarget.value = "";
+      if (depart == "" || depart_time == "") {
+        console.log("empty");
+        this.depart_messageTarget.classList.add("date-error--display")
+        this.depart_messageTarget.classList.remove("date-error--hide")
+        this.depart_messageTarget.innerText = "Fill out all depart information";
+        event.preventDefault();
+        event.stopImmediatePropagation()
+      }
+      else {
+        this.depart_messageTarget.innerText = "";
+        this.depart_messageTarget.classList.remove("date-error--display")
+        this.depart_messageTarget.classList.add("date-error--hide")
+
+      }
+    }
+    // if (no_submit) {
+    //   console.log("no submit");
+    //   event.preventDefault();
+    //   event.stopImmediatePropagation()
+    // }
+    // else {
+    console.log("all is ok");
+    // }
+  }
+}
