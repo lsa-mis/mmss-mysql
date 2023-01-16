e76cc65ef7a073b205cea7f3aa82fb1404c948ae
Margarita Barvinok <brita@umich.edu>
2023-01-16 11:18:03 -0500

    add more filters to travel

--- app/admin/travels.rb
+++ app/admin/travels.rb
@@ -24,6 +24,13 @@ ActiveAdmin.register Travel do
   filter :depart_session, label: "Session of Departure", as: :select, collection: CampOccurrence.no_any_session.map { |s| s.description_with_month_and_day }
   filter :arrival_date
   filter :depart_date, label: "Departure Date"
+  filter :arrival_transport, as: :select, collection: ['Airplane', 'Bus', 'Train', 
+                                          'Automobile - parent or permitted designee is driving me to the University of Michigan campus', 
+                                          'I am a daily MMSS commuter']
+  filter :depart_transport, label: "Departure Transport", as: :select, collection: ['Airplane', 'Bus', 'Train', 
+                                          'Automobile - parent or permitted designee is driving me to the University of Michigan campus', 
+                                          'I am a daily MMSS commuter']
+
 
   index do
     selectable_column
