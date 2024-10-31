0cbf4835c0c1d490669e18e70fe050673ea6db87
Margarita Barvinok <brita@umich.edu>
2024-10-30 22:49:09 -0400

    don't need the helper - use method in model

--- app/helpers/demographics_helper.rb
+++
@@ -1,9 +0,0 @@
-module DemographicsHelper
-  def demographic_description(applicant_detail)
-    if applicant_detail.demographic.blank?
-      'None Selected'
-    else
-      Demographic.find(applicant_detail.demographic).name
-    end
-  end
-end
