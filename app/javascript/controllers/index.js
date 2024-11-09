f2a8e3d86a71400cd3b9d972c9829bb00837f516
rsmokeUM <rsmoke@umich.edu>
2024-11-09 15:31:56 -0500

    update index.js to register Stimulus controllers with improved naming conventions

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,17 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
+
+const context = require.context(".", true, /_controller\.js$/)
+context.keys().forEach((filename) => {
+  const controllerModule = context(filename)
+  const controllerName = filename
+    .replace(/^.\//, '') // Remove leading './'
+    .replace(/_controller\.js$/, '') // Remove trailing '_controller.js'
+    .replace(/\//g, "--") // Replace nested directories with '--'
+    .replace(/_/g, "-") // Replace underscores with dashes
+
+  application.register(controllerName, controllerModule.default)
+  // console.log(`Registering controller: ${controllerName}`)
+
+})
