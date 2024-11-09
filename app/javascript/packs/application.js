818192cb3c8dfb7be75f43161339c4a4a57c8cb0
rsmokeUM <rsmoke@umich.edu>
2024-11-09 15:32:34 -0500

    remove flatpickr initialization from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,4 @@ require("stylesheets/_footer.sass")
 
 import 'controllers'
 
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
-flatpickr(".datepicker", {})
-
 const images = require.context('../images', true)
