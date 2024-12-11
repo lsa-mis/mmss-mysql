228b4a828563442b68c77613b89b9f4dd74f4526
rsmokeUM <rsmoke@umich.edu>
2024-12-11 15:50:16 -0500

    Refactor CampConfiguration model for improved readability and performance
    This commit simplifies the CampConfiguration model by removing unnecessary 'self' references in scopes, streamlining the 'only_one_active_camp' method, and enhancing the 'dup' method for clarity. Additionally, the ransackable_attributes method has been updated to use a more concise array syntax. These changes improve code maintainability and readability.

--- app/models/camp_configuration.rb
+++ app/models/camp_configuration.rb
@@ -34,44 +34,41 @@ class CampConfiguration < ApplicationRecord
 
   scope :active, -> { where(active: true) }
 
-  scope :active_camp_year, -> { self.active.pick(:camp_year) }
-  scope :active_camp_year_application_open, -> { self.active.pick(:application_open) }
-  scope :active_camp_year_application_close, -> { self.active.pick(:application_close) }
-  scope :active_camp_materials_due_date, -> { self.active.pick(:application_materials_due) }
-  scope :active_camp_fee_cents, -> { self.active.pick(:application_fee_cents) }
+  scope :active_camp_year, -> { active.pick(:camp_year) }
+  scope :active_camp_year_application_open, -> { active.pick(:application_open) }
+  scope :active_camp_year_application_close, -> { active.pick(:application_close) }
+  scope :active_camp_materials_due_date, -> { active.pick(:application_materials_due) }
+  scope :active_camp_fee_cents, -> { active.pick(:application_fee_cents) }
 
   def display_name
-    self.camp_year # or whatever column you want
+    camp_year # or whatever column you want
   end
 
   def only_one_active_camp
     return unless active?
-  
+
     matches = CampConfiguration.active
-    if persisted?
-      matches = matches.where('id != ?', id)
-    end
-    if matches.exists?
-      errors.add(:active, 'cannot have another active camp')
-    end
+    matches = matches.where('id != ?', id) if persisted?
+    return unless matches.exists?
+
+    errors.add(:active, 'cannot have another active camp')
   end
 
   def dup
     super.tap do |new_camp_configuration|
-
-      new_camp_configuration.camp_year = ""
-      new_camp_configuration.application_open = ""
-      new_camp_configuration.application_close = ""
-      new_camp_configuration.application_materials_due = ""
-      new_camp_configuration.priority = ""
-      new_camp_configuration.camper_acceptance_due = ""
-      new_camp_configuration.active = ""
-      new_camp_configuration.student_packet_url = ""
+      new_camp_configuration.camp_year = ''
+      new_camp_configuration.application_open = ''
+      new_camp_configuration.application_close = ''
+      new_camp_configuration.application_materials_due = ''
+      new_camp_configuration.priority = ''
+      new_camp_configuration.camper_acceptance_due = ''
+      new_camp_configuration.active = ''
+      new_camp_configuration.student_packet_url = ''
     end
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["active", "application_close", "application_fee_cents", "application_materials_due", "application_open", "camp_year", "camper_acceptance_due", "created_at", "id", "offer_letter", "priority", "reject_letter", "student_packet_url", "updated_at", "waitlist_letter"]
+    %w[active application_close application_fee_cents application_fee_required application_materials_due
+       application_open camp_year camper_acceptance_due created_at id offer_letter priority reject_letter student_packet_url updated_at waitlist_letter]
   end
-
 end
