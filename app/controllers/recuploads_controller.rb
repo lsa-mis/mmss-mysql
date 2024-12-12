9678cd51cd1d6aec4d7c52eff3a32b85e2af42e5
rsmokeUM <rsmoke@umich.edu>
2024-12-12 15:50:27 -0500

    Refactor RecuploadsController for improved clarity and application status handling
    This commit refactors the RecuploadsController by standardizing string delimiters to single quotes for consistency. It enhances the logic in the create action to conditionally mark an enrollment as complete based on the application fee requirement. Additionally, the before_action callbacks have been updated for better readability. Minor whitespace adjustments were made to improve code clarity.

--- app/controllers/recuploads_controller.rb
+++ app/controllers/recuploads_controller.rb
@@ -3,38 +3,33 @@ class RecuploadsController < InheritedResources::Base
   # before_action :authenticate_logged_in!
   # before_action :authenticate_admin!, only: [:index, :destroy]
 
-  before_action :authenticate_admin!, except: [:success, :error, :new, :create]
-  before_action :set_recupload, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!, except: %i[success error new create]
+  before_action :set_recupload, only: %i[show edit update destroy]
   before_action :get_recommendation, only: [:new]
   before_action :get_student, only: [:create]
 
   def index
     redirect_to root_path unless admin_signed_in?
-
   end
 
   def show
-
   end
 
   def error
-
   end
 
   def success
-
   end
 
   def new
     if @recommendation.recupload.present?
-      redirect_to recupload_error_path, alert: "A recommendation has already been submitted for this user"
+      redirect_to recupload_error_path, alert: 'A recommendation has already been submitted for this user'
     else
       @recupload = @recommendation.build_recupload
     end
   end
 
   def edit
-
   end
 
   def create
@@ -48,8 +43,11 @@ class RecuploadsController < InheritedResources::Base
         RecuploadMailer.with(recupload: @recupload).applicant_received_email.deliver_now
         recom_id = @recupload.recommendation_id
         enroll_id = Recommendation.find(recom_id).enrollment_id
-        if Payment.where(user_id: Enrollment.find(enroll_id).user_id).current_camp_payments.exists?
-         Enrollment.find(enroll_id).update!(application_status: "application complete", application_status_updated_on: Date.today)
+        enrollment = Enrollment.find(enroll_id)
+
+        # Mark as complete if either payment exists (when required) or no payment was required
+        if !enrollment.application_fee_required || Payment.where(user_id: enrollment.user_id).current_camp_payments.exists?
+          enrollment.update!(application_status: 'application complete', application_status_updated_on: Date.today)
         end
       else
         format.html { render :new }
@@ -59,7 +57,6 @@ class RecuploadsController < InheritedResources::Base
   end
 
   def update
-
   end
 
   def destroy
@@ -72,23 +69,22 @@ class RecuploadsController < InheritedResources::Base
 
   private
 
-    def set_recupload
-      @recupload = Recupload.find(params[:id])
-    end
+  def set_recupload
+    @recupload = Recupload.find(params[:id])
+  end
 
-    def get_recommendation
-      hash_val = params['hash']
-      rec_id = hash_val.split("nGklDoc2egIkzFxr0U").last.to_i
-      @recommendation = Recommendation.find(rec_id)
-      @student = ApplicantDetail.find(params[:id]).full_name
-    end
+  def get_recommendation
+    hash_val = params['hash']
+    rec_id = hash_val.split('nGklDoc2egIkzFxr0U').last.to_i
+    @recommendation = Recommendation.find(rec_id)
+    @student = ApplicantDetail.find(params[:id]).full_name
+  end
 
-    def get_student
-      @student = params[:recupload]['studentname']
-    end
+  def get_student
+    @student = params[:recupload]['studentname']
+  end
 
-    def recupload_params
-      params.require(:recupload).permit(:letter, :authorname, :studentname, :recommendation_id, :rechash, :recletter)
-    end
-
+  def recupload_params
+    params.require(:recupload).permit(:letter, :authorname, :studentname, :recommendation_id, :rechash, :recletter)
+  end
 end
