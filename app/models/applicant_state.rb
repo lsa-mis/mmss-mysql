fd8c371adaa11158f58fe35f79b11b861ac1efac
rsmokeUM <rsmoke@umich.edu>
2024-12-12 15:49:46 -0500

    Refactor ApplicantState model to improve total_cost calculation and standardize string delimiters. The total_cost method now conditionally includes the application fee based on the current enrollment's requirement, enhancing cost accuracy. Additionally, string delimiters have been standardized from double quotes to single quotes for consistency across methods. Minor adjustments were made to maintain code clarity and efficiency.

--- app/models/applicant_state.rb
+++ app/models/applicant_state.rb
@@ -1,10 +1,9 @@
 module ApplicantState
-
-  def registration_activities 
+  def registration_activities
     @current_enrollment.registration_activities.order(camp_occurrence_id: :asc)
   end
 
-  def session_registrations 
+  def session_registrations
     @current_enrollment.session_registrations.order(description: :asc)
   end
 
@@ -17,7 +16,7 @@ module ApplicantState
   end
 
   def has_any_session
-    @session_registrations.pluck(:description).include?("Any Session")
+    @session_registrations.pluck(:description).include?('Any Session')
   end
 
   def finaids_ttl
@@ -25,7 +24,7 @@ module ApplicantState
   end
 
   def finaids_awarded_ttl
-    finaids.where(status: "awarded").pluck(:amount_cents).sum
+    finaids.where(status: 'awarded').pluck(:amount_cents).sum
   end
 
   def users_current_payments
@@ -36,20 +35,31 @@ module ApplicantState
     @current_enrollment.user.payments.current_camp_payments.where(transaction_status: '1').pluck(:total_amount).map(&:to_i).sum
   end
 
+  # def total_cost
+  #   cost_sessions_ttl + cost_activities_ttl + CampConfiguration.active_camp_fee_cents
+  # end
   def total_cost
-    cost_sessions_ttl + cost_activities_ttl + CampConfiguration.active_camp_fee_cents
+    base_cost = cost_sessions_ttl + cost_activities_ttl
+    active_camp = CampConfiguration.active.first
+
+    # Include application fee only if it was required for this enrollment
+    if @current_enrollment.application_fee_required
+      base_cost + active_camp.application_fee_cents
+    else
+      base_cost
+    end
   end
 
   def balance_due
     total_cost.to_i - finaids_awarded_ttl.to_i - ttl_paid.to_i
   end
 
-  def assigned_sessions_ids 
-    SessionAssignment.where(enrollment_id: @current_enrollment, offer_status: "accepted").pluck(:camp_occurrence_id)
+  def assigned_sessions_ids
+    SessionAssignment.where(enrollment_id: @current_enrollment, offer_status: 'accepted').pluck(:camp_occurrence_id)
   end
 
   def cost_sessions_ttl
-    # sum of each session use is assigned to 
+    # sum of each session use is assigned to
     CampOccurrence.where(id: assigned_sessions_ids).pluck(:cost_cents).sum
   end
 
@@ -57,5 +67,4 @@ module ApplicantState
     activity_ids = EnrollmentActivity.where(enrollment_id: @current_enrollment).pluck(:activity_id)
     Activity.where(id: activity_ids, camp_occurrence_id: assigned_sessions_ids).pluck(:cost_cents).sum
   end
-
 end
