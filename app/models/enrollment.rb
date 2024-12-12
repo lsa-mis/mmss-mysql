98d38cede64ad3bf3106776c5b4b950aa7fdd2a9
rsmokeUM <rsmoke@umich.edu>
2024-12-12 15:50:17 -0500

    Enhance Enrollment model with application fee logic and refactor scopes for clarity
    This commit introduces a new callback to set the application_fee_required attribute during enrollment creation, ensuring accurate fee management based on the active camp's configuration. Additionally, several scopes have been refactored for improved readability and consistency, transitioning to lambda syntax where appropriate. Minor adjustments were made to error handling and string delimiters for uniformity across the model.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -33,6 +33,7 @@ class Enrollment < ApplicationRecord
   before_update :if_camp_doc_form_completed
   before_update :if_application_status_changed
   before_update :set_application_deadline
+  before_create :set_application_fee_required
   after_update :send_offer_letter
   after_commit :send_enroll_letter, if: :persisted?
   after_commit :send_rejected_letter, if: :persisted?
@@ -44,7 +45,7 @@ class Enrollment < ApplicationRecord
   has_many :enrollment_activities, dependent: :destroy
   has_many :registration_activities, through: :enrollment_activities, source: :activity
 
-  has_many :session_activities, dependent: :destroy 
+  has_many :session_activities, dependent: :destroy
   has_many :session_registrations, through: :session_activities, source: :camp_occurrence
 
   has_many :session_assignments, dependent: :destroy
@@ -92,142 +93,152 @@ class Enrollment < ApplicationRecord
   scope :offered, -> { current_camp_year_applications.where("offer_status = 'offered'") }
   scope :accepted, -> { current_camp_year_applications.where("offer_status = 'accepted'") }
   scope :enrolled, -> { current_camp_year_applications.where("application_status = 'enrolled'") }
-  scope :application_complete, -> { current_camp_year_applications.where("application_status = 'application complete'") }
-  scope :application_complete_not_offered, -> { application_complete.where(offer_status: [nil, ""])}
+  scope :application_complete, lambda {
+    current_camp_year_applications.where("application_status = 'application complete'")
+  }
+  scope :application_complete_not_offered, -> { application_complete.where(offer_status: [nil, '']) }
   scope :no_recomendation, -> { current_camp_year_applications.where.missing(:recommendation) }
-  scope :no_letter, -> { current_camp_year_applications.where(id: Recommendation.where.missing(:recupload).pluck(:enrollment_id)) }
-  scope :no_payments, -> { current_camp_year_applications.where.not(user_id: Payment.where(camp_year: CampConfiguration.active.last.camp_year).pluck(:user_id)) }
-  scope :no_student_packet, -> { current_camp_year_applications.where.not(id: Enrollment.current_camp_year_applications.joins(:student_packet_attachment).pluck(:id)) }
-  scope :no_vaccine_record, -> { enrolled.where.not(id: Enrollment.current_camp_year_applications.joins(:vaccine_record_attachment).pluck(:id)) }
-  scope :no_covid_test_record, -> { enrolled.where.not(id: Enrollment.current_camp_year_applications.joins(:covid_test_record_attachment).pluck(:id)) }
+  scope :no_letter, lambda {
+    current_camp_year_applications.where(id: Recommendation.where.missing(:recupload).pluck(:enrollment_id))
+  }
+  scope :no_payments, lambda {
+    current_camp_year_applications.where.not(user_id: Payment.where(camp_year: CampConfiguration.active.last.camp_year).pluck(:user_id))
+  }
+  scope :no_student_packet, lambda {
+    current_camp_year_applications.where.not(id: Enrollment.current_camp_year_applications.joins(:student_packet_attachment).pluck(:id))
+  }
+  scope :no_vaccine_record, lambda {
+    enrolled.where.not(id: Enrollment.current_camp_year_applications.joins(:vaccine_record_attachment).pluck(:id))
+  }
+  scope :no_covid_test_record, lambda {
+    enrolled.where.not(id: Enrollment.current_camp_year_applications.joins(:covid_test_record_attachment).pluck(:id))
+  }
   scope :no_camp_doc_form, -> { current_camp_year_applications.where(camp_doc_form_completed: false) }
 
   def display_name
-    "#{self.applicant_detail.full_name} - #{self.user.email}"
+    "#{applicant_detail.full_name} - #{user.email}"
   end
 
   def last_name
-    "#{self.applicant_detail.lastname} - #{self.user.email}"
+    "#{applicant_detail.lastname} - #{user.email}"
   end
 
   private
 
   def at_least_one_session_is_checked
-    if session_registration_ids.empty?
-      errors.add(:base, "Select at least one session")
-    end
+    return unless session_registration_ids.empty?
+
+    errors.add(:base, 'Select at least one session')
   end
 
   def at_least_one_course_is_checked
-    if course_registration_ids.empty?
-      errors.add(:base, "Select at least one course")
-    end
+    return unless course_registration_ids.empty?
+
+    errors.add(:base, 'Select at least one course')
   end
 
   def validate_transcript_presence
-    errors.add(:transcript, 'should exist') unless self.transcript.attached?
+    errors.add(:transcript, 'should exist') unless transcript.attached?
   end
 
   def acceptable_transcript
     return unless transcript.attached?
 
     unless transcript.blob.byte_size <= 20.megabyte
-      errors.add(:transcript, "is too big - file size cannot exceed 20Mbyte")
+      errors.add(:transcript, 'is too big - file size cannot exceed 20Mbyte')
     end
 
-    acceptable_types = ["image/png", "image/jpeg", "application/pdf"]
-    unless acceptable_types.include?(transcript.content_type)
-      errors.add(:transcript, "must be file type PDF, JPEG or PNG")
-    end
+    acceptable_types = ['image/png', 'image/jpeg', 'application/pdf']
+    return if acceptable_types.include?(transcript.content_type)
+
+    errors.add(:transcript, 'must be file type PDF, JPEG or PNG')
   end
 
   def acceptable_student_packet
     return unless student_packet.attached?
 
     unless student_packet.blob.byte_size <= 20.megabyte
-      errors.add(:student_packet, "is too big - file size cannot exceed 20Mbyte")
+      errors.add(:student_packet, 'is too big - file size cannot exceed 20Mbyte')
     end
 
-    acceptable_types = ["image/png", "image/jpeg", "application/pdf"]
-    unless acceptable_types.include?(student_packet.content_type)
-      errors.add(:student_packet, "must be file type PDF, JPEG or PNG")
-    end
+    acceptable_types = ['image/png', 'image/jpeg', 'application/pdf']
+    return if acceptable_types.include?(student_packet.content_type)
+
+    errors.add(:student_packet, 'must be file type PDF, JPEG or PNG')
   end
 
   def acceptable_image
     return unless covid_test_record.attached? || vaccine_record.attached?
 
     [covid_test_record, vaccine_record].compact.each do |image|
+      next unless image.attached?
 
-      if image.attached?
-        unless image.blob.byte_size <= 10.megabyte
-          errors.add(image.name, "is too big")
-        end
+      errors.add(image.name, 'is too big') unless image.blob.byte_size <= 10.megabyte
 
-        acceptable_types = ["image/png", "image/jpeg", "application/pdf"]
-        unless acceptable_types.include?(image.content_type)
-          errors.add(image.name, "incorrect file type")
-        end
-      end
+      acceptable_types = ['image/png', 'image/jpeg', 'application/pdf']
+      errors.add(image.name, 'incorrect file type') unless acceptable_types.include?(image.content_type)
     end
   end
 
   def if_camp_doc_form_completed
     payment = PaymentState.new(self)
-    if self.camp_doc_form_completed && payment.balance_due == 0
-      self.application_status = "enrolled"
-    end
+    return unless camp_doc_form_completed && payment.balance_due == 0
+
+    self.application_status = 'enrolled'
   end
 
   def send_offer_letter
-    if previous_changes[:offer_status]
-      if self.offer_status == "offered"
-        OfferMailer.offer_email(self.user_id).deliver_now
-      end
-    end
+    return unless previous_changes[:offer_status]
+    return unless offer_status == 'offered'
+
+    OfferMailer.offer_email(user_id).deliver_now
   end
 
   def send_enroll_letter
-    if previous_changes[:application_status]
-      if self.application_status == "enrolled"
-        RegistrationMailer.app_enrolled_email(self.user).deliver_now
-      end
-    end
+    return unless previous_changes[:application_status]
+    return unless application_status == 'enrolled'
+
+    RegistrationMailer.app_enrolled_email(user).deliver_now
   end
 
   def send_rejected_letter
-    if previous_changes[:application_status]
-      if self.application_status == "rejected"
-        RejectedMailer.app_rejected_email(self).deliver_now
-      end
-    end
+    return unless previous_changes[:application_status]
+    return unless application_status == 'rejected'
+
+    RejectedMailer.app_rejected_email(self).deliver_now
   end
 
   def send_waitlisted_letter
-    if previous_changes[:application_status]
-      if self.application_status == "waitlisted"
-        WaitlistedMailer.app_waitlisted_email(self).deliver_now
-      end
-    end
+    return unless previous_changes[:application_status]
+    return unless application_status == 'waitlisted'
+
+    WaitlistedMailer.app_waitlisted_email(self).deliver_now
   end
 
   def set_application_deadline
-    if self.session_assignments.present? && self.course_assignments.present?
-      self.application_deadline = 30.days.from_now unless self.application_deadline.present?
-    end
+    return unless session_assignments.present? && course_assignments.present?
+
+    self.application_deadline = 30.days.from_now unless application_deadline.present?
   end
 
   def if_application_status_changed
-    if self.application_status_changed?
-      self.application_status_updated_on = Date.today
-    end
+    return unless application_status_changed?
+
+    self.application_status_updated_on = Date.today
+  end
+
+  def set_application_fee_required
+    active_camp = CampConfiguration.active.first
+    self.application_fee_required = active_camp&.application_fee_required || false
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["anticipated_graduation_year", "application_deadline", "application_status", "application_status_updated_on", "campyear", "created_at", "high_school_address1", "high_school_address2", "high_school_city", "high_school_country", "high_school_name", "high_school_non_us", "high_school_postalcode", "high_school_state", "id", "international", "notes", "offer_status", "partner_program", "personal_statement", "room_mate_request", "uniqname", "updated_at", "user_id", "year_in_school"]
+    %w[anticipated_graduation_year application_deadline application_status application_status_updated_on
+       campyear created_at high_school_address1 high_school_address2 high_school_city high_school_country high_school_name high_school_non_us high_school_postalcode high_school_state id international notes offer_status partner_program personal_statement room_mate_request uniqname updated_at user_id year_in_school]
   end
 
   def self.ransackable_associations(auth_object = nil)
-    ["applicant_detail", "course_assignments", "course_preferences", "course_registrations", "covid_test_record_attachment", "covid_test_record_blob", "enrollment_activities", "financial_aids", "recommendation", "registration_activities", "rejection", "session_activities", "session_assignments", "session_registrations", "student_packet_attachment", "student_packet_blob", "transcript_attachment", "transcript_blob", "travels", "user", "vaccine_record_attachment", "vaccine_record_blob"]
+    %w[applicant_detail course_assignments course_preferences course_registrations
+       covid_test_record_attachment covid_test_record_blob enrollment_activities financial_aids recommendation registration_activities rejection session_activities session_assignments session_registrations student_packet_attachment student_packet_blob transcript_attachment transcript_blob travels user vaccine_record_attachment vaccine_record_blob]
   end
 end
