be9f47eff72630edb6537650ad23c40c11d53c29
rsmokeUM <rsmoke@umich.edu>
2025-03-17 11:51:47 -0400

    Refactor RecommendationMailer to disable Sendgrid click tracking and streamline URL construction for recommendation requests.

--- app/mailers/recommendation_mailer.rb
+++ app/mailers/recommendation_mailer.rb
@@ -1,5 +1,4 @@
 class RecommendationMailer < ApplicationMailer
-
   default from: 'University of Michigan MMSS High School Summer Program <mmss-recommend@umich.edu>'
 
   def request_email
@@ -7,8 +6,12 @@ class RecommendationMailer < ApplicationMailer
     @enrollment = Enrollment.find(@recommendation.enrollment_id)
     @student = ApplicantDetail.find_by(user_id: @enrollment.user_id)
     @hashval = "nGklDoc2egIkzFxr0U#{@recommendation.id}" # sha256_hash
-    @url  = "#{ ConstantData::HOST_URL }/recuploads/new?id=#{ @student.id }&yr=#{ CampConfiguration.active_camp_year }&email=#{ @recommendation.email }&hash=#{ @hashval }"
-    mail(to: @recommendation.email, subject: "Recommendation request for #{ @student.firstname } #{ @student.lastname }")
-  end
+    @url = "#{ConstantData::HOST_URL}/recuploads/new?id=#{@student.id}&yr=#{CampConfiguration.active_camp_year}&email=#{@recommendation.email}&hash=#{@hashval}"
 
+    # Disable Sendgrid click tracking for this email
+    headers['X-SMTPAPI'] = '{"filters":{"clicktrack":{"settings":{"enable":0}}}}'
+
+    mail(to: @recommendation.email,
+         subject: "Recommendation request for #{@student.firstname} #{@student.lastname}")
+  end
 end
