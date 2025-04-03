692f25fab3298235a15d61e1b26b2ddea68cb72c
rsmokeUM <rsmoke@umich.edu>
2025-04-03 16:40:37 -0400

    Update Devise mailer sender configuration for clarity and consistency. Commented out the previous sender email and noted that it is set in the application_mailer.rb file.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'mmss-support@umich.edu'
+  # config.mailer_sender = 'no-reply@math.lsa.umich.edu'
+  # # NOTE: this is set in the application_mailer.rb file
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
