52d49e0052c615c3c3555cf5a60c82cb3d74db66
rsmokeUM <rsmoke@umich.edu>
2025-04-08 15:08:34 -0400

    Enhance FeedbackMailer with improved email formatting and add preview functionality
    This commit updates the FeedbackMailer by enhancing the HTML and text email templates for feedback notifications, providing a clearer structure and better presentation of feedback details. Additionally, a new FeedbackMailerPreview class is introduced to facilitate testing and previewing of feedback emails in the development environment.

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,10 +1,8 @@
 class FeedbackMailer < ApplicationMailer
-
   def feedback_email
     @url = ConstantData::HOST_URL
     @feedback = params[:feedback]
     @sender = User.find(@feedback.user_id)
     mail(to: 'mmss-support@umich.edu', subject: "Feedback from #{@sender.email}")
   end
-
 end
