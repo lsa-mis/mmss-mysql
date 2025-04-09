7324be33a975281b867e8ad604fcde6f1508e46f
rsmokeUM <rsmoke@umich.edu>
2025-04-09 18:50:31 -0400

    Add default URL options to ApplicationMailer for environment-specific configurations
    This commit introduces a new method, default_url_options, in the ApplicationMailer class. It sets the host and protocol based on the Rails environment, ensuring that the application uses the correct URL settings for production and development environments. This enhancement improves the flexibility and reliability of email links sent from the application.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,4 +6,12 @@ class ApplicationMailer < ActionMailer::Base
           reply_to: 'MMSS Support <mmss-support@umich.edu>'
 
   layout 'mailer'
+
+  def default_url_options
+    if Rails.env.production?
+      { host: 'mmss-registration.math.lsa.umich.edu', protocol: 'https' }
+    else
+      { host: 'localhost:3000', protocol: 'http' }
+    end
+  end
 end
