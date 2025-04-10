dd2a0d89750449731263e3ffe805215e5847d7c0
rsmokeUM <rsmoke@umich.edu>
2025-04-10 15:32:42 -0400

    Add course assignment associations and offer management methods in SessionAssignment model
    This commit introduces new associations for course assignments and waitlist assignments in the SessionAssignment model, enhancing the relationship with the Enrollment model. Additionally, it adds methods for accepting and declining session offers, which update the offer status and trigger relevant actions in the CourseAssignment and Enrollment models. These changes improve the management of session offers and streamline the enrollment process.

--- app/models/session_assignment.rb
+++ app/models/session_assignment.rb
@@ -13,8 +13,32 @@ class SessionAssignment < ApplicationRecord
   belongs_to :enrollment
   belongs_to :camp_occurrence
 
+  # Course assignments for this session
+  has_many :course_assignments, through: :enrollment
+  has_many :wait_list_assignments, -> { where(wait_list: true) },
+           through: :enrollment,
+           source: :course_assignments
+
   scope :current_year_session_assignments, -> { where(enrollment_id: Enrollment.current_camp_year_applications) }
   scope :accepted, -> { current_year_session_assignments.where(offer_status: 'accepted') }
+
+  def accept_offer!(user)
+    transaction do
+      update!(offer_status: "accepted")
+      CourseAssignment.handle_session_acceptance(self, user)
+      enrollment.update_status_based_on_session_assignments!
+    end
+  end
+
+  def decline_offer!(user)
+    transaction do
+      update!(offer_status: "declined")
+      CourseAssignment.handle_session_declination(self, user)
+      enrollment.update_status_based_on_session_assignments!
+    end
+  end
+
+  private
 
   def self.ransackable_associations(auth_object = nil)
     ["camp_occurrence", "enrollment"]
