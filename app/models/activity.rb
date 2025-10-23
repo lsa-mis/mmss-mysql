e88af057d11f1add6d5dc72768160c7519223c3c
rsmokeUM <rsmoke@umich.edu>
2025-10-22 20:06:45 -0400

    Update Activity model to enforce dependent destroy on enrollment activities
    - Added `dependent: :destroy` to the `has_many :enrollment_activities` association in the Activity model to ensure that associated enrollment activities are automatically deleted when an Activity is destroyed, preventing orphaned records.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -16,7 +16,7 @@
 class Activity < ApplicationRecord
   include MoneyRails::ActionViewExtension
   belongs_to :camp_occurrence
-  has_many :enrollment_activities
+  has_many :enrollment_activities, dependent: :destroy
   has_many :enrolled_users, through: :enrollment_activities, source: :enrollment
 
   monetize :cost_cents
